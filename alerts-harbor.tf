cb42e14195ead0482b9378b387cd830f9d749738
hiroya-sano <hiroya.sano@ulsystems.co.jp>
2025-05-16 16:17:12 +0900

    アラート条件の追加

--- alerts-harbor.tf
+++ alerts-harbor.tf
@@ -8,6 +8,7 @@ locals {
     cpu_and_memory = "clusterName LIKE 'g3cshrwc-shared-%' AND namespaceName LIKE 'app1986-harbor-%'"
     status = "namespace_name LIKE '%harbor%' or namespace_name LIKE '%jenkins%' or namespace_name LIKE '%vault%'"
     url = "URL = 'https://harbor-${var.harbor_env}.g3cshr.mazda.co.jp'"
+    pvc = "clusterName LIKE 'g3cshrwc-shared-%' AND namespaceName LIKE 'app1986-harbor-%'"
   }
   harbor_threshold = {
     cpu_and_memory = {
@@ -20,21 +21,28 @@ locals {
     url = {
       critical = 1
     }
+    pvc = {
+      critical = 90
+      warning = 80
+    }
   }
   harbor_alert_frequency = {
     cpu_and_memory = 86400
     status = 600
     url = 600
+    pvc = 86400
   }
   harbor_threshold_duration = {
     cpu_and_memory = 300
     status = 300
     url = 300
+    pvc = 300
   }
   harbor_violation_time_limit_seconds = {
     cpu_and_memory = local.harbor_alert_frequency.cpu_and_memory - local.harbor_threshold_duration.cpu_and_memory
     status = local.harbor_alert_frequency.status - local.harbor_threshold_duration.status
     url = local.harbor_alert_frequency.url - local.harbor_threshold_duration.url
+    pvc = local.harbor_alert_frequency.pvc - local.harbor_threshold_duration.pvc
   }
 }
 
@@ -197,7 +205,7 @@ resource "newrelic_nrql_alert_condition" "status_pod_harbor" {
 
 
 resource "newrelic_alert_policy" "status_container_harbor" {
-  name = "Container Status"
+  name = "Container Status by Shared Cluster"
 }
 
 resource "newrelic_nrql_alert_condition" "status_container_harbor" {
@@ -243,6 +251,39 @@ resource "newrelic_nrql_alert_condition" "url_harbor" {
     operator              = "above"
     threshold             = local.harbor_threshold.url.critical
     threshold_duration    = local.harbor_threshold_duration.url
+    threshold_occurrences = "ALL"
+  }
+}
+
+
+resource "newrelic_alert_policy" "pvcrate_harbor" {
+  name = "PVC by Harbor"
+}
+
+resource "newrelic_nrql_alert_condition" "pvcrate_harbor" {
+  policy_id = newrelic_alert_policy.pvcrate_harbor.id
+  name      = "PVC by Harbor Usage Rate (%)"
+  enabled = local.alert_enabled
+  aggregation_method = "event_flow"
+  aggregation_window = "60"
+  aggregation_delay = "60"
+  violation_time_limit_seconds = local.harbor_violation_time_limit_seconds.pvc
+
+  nrql {
+    query      = "FROM K8sVolumeSample SELECT latest(fsUsedPercent) FACET clusterName WHERE ${local.harbor_query_filter.pvc}"
+  }
+
+  critical {
+    operator              = "above"
+    threshold             = local.harbor_threshold.pvc.critical
+    threshold_duration    = local.harbor_threshold_duration.pvc
+    threshold_occurrences = "ALL"
+  }
+
+  warning {
+    operator              = "above"
+    threshold             = local.harbor_threshold.pvc.warning
+    threshold_duration    = local.harbor_threshold_duration.pvc
     threshold_occurrences = "ALL"
   }
 }
--- alerts-infra.tf
+++ alerts-infra.tf
@@ -1,12 +1,37 @@
 locals {
-  infra_query_filter = "clusterName NOT LIKE 'g3stmt%' and clusterName NOT LIKE 'g3cshrwc-shared-dev' and clusterName NOT LIKE 'g3cshrwc-shared-prod'"
-  infra_threshold = {
-    critical = 90
-    warning = 80
+  infra_query_filter = {
+    cpu_and_memory = "clusterName NOT LIKE 'g3stmt%' and clusterName NOT LIKE 'g3cshrwc-shared-dev' and clusterName NOT LIKE 'g3cshrwc-shared-prod'"
+    disk = "clusterName NOT LIKE 'g3stmt%'"
+    state = "clusterName NOT LIKE 'g3stmt%'"
   }
-  infra_alert_frequency = 600
-  infra_threshold_duration = 300
-  infra_violation_time_limit_seconds = local.infra_alert_frequency - local.infra_threshold_duration
+  infra_threshold = {
+    cpu_and_memory = {
+      critical = 90
+      warning = 80
+    }
+    disk = {
+      critical = 72.5
+      warning = 65
+    }
+    state = {
+      critical = 1
+    }
+  }
+  infra_alert_frequency = {
+    cpu_and_memory = 600
+    disk = 600
+    state = 480
+  }
+  infra_threshold_duration = {
+    cpu_and_memory = 300
+    disk = 300
+    state = 180
+  }
+  infra_violation_time_limit_seconds = {
+    cpu_and_memory = local.infra_alert_frequency.cpu_and_memory - local.infra_threshold_duration.cpu_and_memory
+    disk = local.infra_alert_frequency.disk - local.infra_threshold_duration.disk
+    state = local.infra_alert_frequency.state - local.infra_threshold_duration.state
+  }
 }
 
 resource "newrelic_alert_policy" "cpurate_infra" {
@@ -20,23 +45,23 @@ resource "newrelic_nrql_alert_condition" "cpurate_infra" {
   aggregation_method = "event_flow"
   aggregation_window = "60"
   aggregation_delay = "60"
-  violation_time_limit_seconds = local.infra_violation_time_limit_seconds
+  violation_time_limit_seconds = local.infra_violation_time_limit_seconds.cpu_and_memory
 
   nrql {
-    query      = "FROM K8sContainerSample SELECT average(cpuCoresUtilization) FACET clusterName WHERE ${local.infra_query_filter}"
+    query      = "FROM K8sContainerSample SELECT average(cpuCoresUtilization) FACET clusterName WHERE ${local.infra_query_filter.cpu_and_memory}"
   }
 
   critical {
     operator              = "above"
-    threshold             = local.infra_threshold.critical
-    threshold_duration    = local.infra_threshold_duration
+    threshold             = local.infra_threshold.cpu_and_memory.critical
+    threshold_duration    = local.infra_threshold_duration.cpu_and_memory
     threshold_occurrences = "ALL"
   }
 
   warning {
     operator              = "above"
-    threshold             = local.infra_threshold.warning
-    threshold_duration    = local.infra_threshold_duration
+    threshold             = local.infra_threshold.cpu_and_memory.warning
+    threshold_duration    = local.infra_threshold_duration.cpu_and_memory
     threshold_occurrences = "ALL"
   }
 }
@@ -53,23 +78,89 @@ resource "newrelic_nrql_alert_condition" "memoryrate_infra" {
   aggregation_method = "event_flow"
   aggregation_window = "60"
   aggregation_delay = "60"
-  violation_time_limit_seconds = local.infra_violation_time_limit_seconds
+  violation_time_limit_seconds = local.infra_violation_time_limit_seconds.cpu_and_memory
 
   nrql {
-    query      = "FROM K8sContainerSample SELECT average(memoryUtilization) FACET clusterName WHERE ${local.infra_query_filter}"
+    query      = "FROM K8sContainerSample SELECT average(memoryUtilization) FACET clusterName WHERE ${local.infra_query_filter.cpu_and_memory}"
   }
 
   critical {
     operator              = "above"
-    threshold             = local.infra_threshold.critical
-    threshold_duration    = local.infra_threshold_duration
+    threshold             = local.infra_threshold.cpu_and_memory.critical
+    threshold_duration    = local.infra_threshold_duration.cpu_and_memory
     threshold_occurrences = "ALL"
   }
 
   warning {
     operator              = "above"
-    threshold             = local.infra_threshold.warning
-    threshold_duration    = local.infra_threshold_duration
+    threshold             = local.infra_threshold.cpu_and_memory.warning
+    threshold_duration    = local.infra_threshold_duration.cpu_and_memory
+    threshold_occurrences = "ALL"
+  }
+}
+
+
+resource "newrelic_alert_policy" "diskrate_infra" {
+  name = "Disk Usage Rate Policy by Node"
+}
+
+resource "newrelic_nrql_alert_condition" "diskrate_infra" {
+  policy_id = newrelic_alert_policy.diskrate_infra.id
+  name      = "Disk Usage Rate (%) by Node"
+  enabled = local.alert_enabled
+  aggregation_method = "event_flow"
+  aggregation_window = "60"
+  aggregation_delay = "60"
+  violation_time_limit_seconds = local.infra_violation_time_limit_seconds.disk
+
+  nrql {
+    query      = "FROM K8sNodeSample SELECT latest(fsCapacityUtilization) FACET nodeName WHERE ${local.infra_query_filter.disk}"
+  }
+
+  critical {
+    operator              = "above"
+    threshold             = local.infra_threshold.disk.critical
+    threshold_duration    = local.infra_threshold_duration.disk
+    threshold_occurrences = "ALL"
+  }
+
+  warning {
+    operator              = "above"
+    threshold             = local.infra_threshold.disk.warning
+    threshold_duration    = local.infra_threshold_duration.disk
+    threshold_occurrences = "ALL"
+  }
+}
+
+
+resource "newrelic_alert_policy" "state_infra" {
+  name = "Not Ready Status by Node"
+}
+
+resource "newrelic_nrql_alert_condition" "state_infra" {
+  policy_id = newrelic_alert_policy.state_infra.id
+  name      = "Not Ready Status by Node"
+  enabled = local.alert_enabled
+  aggregation_method = "event_flow"
+  aggregation_window = "60"
+  aggregation_delay = "60"
+  violation_time_limit_seconds = local.infra_violation_time_limit_seconds.state
+
+  nrql {
+    query      = "FROM K8sNodeSample SELECT uniqueCount(nodeName) FACET nodeName WHERE condition.Ready != 1 AND ${local.infra_query_filter.disk}"
+  }
+
+  critical {
+    operator              = "above"
+    threshold             = local.infra_threshold.disk.critical
+    threshold_duration    = local.infra_threshold_duration.disk
+    threshold_occurrences = "ALL"
+  }
+
+  warning {
+    operator              = "above"
+    threshold             = local.infra_threshold.disk.warning
+    threshold_duration    = local.infra_threshold_duration.disk
     threshold_occurrences = "ALL"
   }
 }
